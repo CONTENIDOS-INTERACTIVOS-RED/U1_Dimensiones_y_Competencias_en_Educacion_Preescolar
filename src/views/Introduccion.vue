8dfb6ab2e450516517ec324e7b0da4164c55aa1e
Francisco Lizcano <flizcanor@icloud.com>
2025-02-14 07:38:12 -0500

    Intro y Portada

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,6 +1,27 @@
 <template lang="pug">
-.curso-main-container.introduccion
-  BannerInterno(subTitulo="Introducción")
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .curso-main-container.introduccion
+    BannerInterno(subTitulo="Introducción")
+    .container.tarjeta.tarjeta--blanca.px-4.px-md-5.pt-md-5.pt-4
+  
+      .row.mb-5
+        .col-lg-8
+  
+          .bg-color-1.p-4(data-aos="fade-left").mb-4
+            p.mb-0 En esta unidad, se abordarán los #[b lineamientos curriculares] y los #[b estándares básicos de aprendizaje (DBA)] establecidos por el Ministerio de Educación Nacional (MEN) para la educación preescolar. Estos lineamientos y estándares son fundamentales para el diseño y desarrollo de procesos educativos que favorezcan el desarrollo integral de los niños en su primera infancia. Comprender los principios que guían estos lineamientos es esencial para construir un currículo inclusivo, adaptado a las diversas necesidades y características de los estudiantes.
+          
+          p(data-aos="fade-right") Al finalizar esta unidad, se espera que se logre un análisis y reconocimiento de los criterios que sustentan los lineamientos y estándares, así como de la relación de estos con las #[b inteligencias múltiples] y su aplicación práctica en el aula preescolar. A través de este análisis, se adquirirán herramientas que permitirán implementar estrategias educativas efectivas, orientadas a promover el aprendizaje y el desarrollo de competencias en los niños.
+  
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
+  
+      .bg-full-width.bg-color-2
+        .px-4.px-md-5.py-4
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/intro/2.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-2 Durante esta unidad, se trabajará sobre temas como los estándares básicos de aprendizaje, su coherencia vertical y horizontal, y la forma en que los lineamientos curriculares pueden integrarse en las actividades pedagógicas. Además, se reflexionará sobre el impacto de las inteligencias múltiples en el diseño curricular y cómo estas pueden ser aplicadas para favorecer el aprendizaje de cada niño según sus capacidades y estilos de aprendizaje.
 
-</template>
+              p.mb-0 #[b ¡Se inicia este viaje de aprendizaje!].
+  </template>
